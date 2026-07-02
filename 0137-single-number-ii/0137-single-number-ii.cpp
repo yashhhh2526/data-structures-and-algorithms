8a18be9da778e5054e3ade3cdfa5e415156b47a0
yashhhh2526 <chodipalliyaswanth24bcs0274@iiitkottayam.ac.in>
2026-07-02 19:16:11 +0530

    Time: 0 ms (100%), Space: 13.2 MB (99.25%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int i = 0;
-        while(i<n){
-            if(i+1<n && nums[i] == nums[i+1]){
-                i = i+3;
-            }else{
-                return nums[i];
-                break;
-            }
+        int ones = 0, twos = 0;
+
+        for (int num : nums) {
+            ones = (ones ^ num) & (~twos);
+            twos = (twos ^ num) & (~ones);
         }
-       return -1;
+
+        return ones;
     }
 };
