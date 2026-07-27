24391b12306b2679ea64436af8caf2d64fa02505
yashhhh2526 <chodipalliyaswanth24bcs0274@iiitkottayam.ac.in>
2026-07-27 18:57:21 +0530

    Time: 0 ms (100%), Space: 7.9 MB (93.84%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0;
+        int high = 0;
+        for(char c : s){
+            if(c == '('){
+                low++;
+                high++;
+            }else if(c == ')'){
+                low--;
+                high--;    
+            }else{
+                low --;
+                high++;
+            }
+            if(low<0){
+                low = 0;
+            }
+            if(high < 0){
+                return false;
+            }
+        }
+        return (low == 0);
+    }
+};
