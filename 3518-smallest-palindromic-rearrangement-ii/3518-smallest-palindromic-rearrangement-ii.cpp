5c6d1f508d00bba10cc627aa9a94c24cbd02e0d4
yashhhh2526 <chodipalliyaswanth24bcs0274@iiitkottayam.ac.in>
2026-07-29 11:15:50 +0530

    Time: 25 ms (62.32%), Space: 17.4 MB (36.23%) - LeetHub

---
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    const long long LIM = 1000000LL + 5;
+
+    // nCr capped at LIM
+    long long C(int n, int r) {
+        if (r > n) return 0;
+        r = min(r, n - r);
+
+        long long ans = 1;
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (n - r + i) / i;
+            if (ans >= LIM) return LIM;
+        }
+        return ans;
+    }
+
+    // Number of distinct permutations of the multiset
+    long long countWays(vector<int>& cnt) {
+        int total = 0;
+        for (int x : cnt) total += x;
+
+        long long ways = 1;
+        int rem = total;
+
+        for (int x : cnt) {
+            if (x == 0) continue;
+            ways *= C(rem, x);
+            if (ways >= LIM) return LIM;
+            rem -= x;
+        }
+
+        return ways;
+    }
+
+    string smallestPalindrome(string s, int k) {
+
+        vector<int> freq(26, 0);
+
+        for (char c : s)
+            freq[c - 'a']++;
+
+        vector<int> half(26, 0);
+        string mid = "";
+
+        for (int i = 0; i < 26; i++) {
+            half[i] = freq[i] / 2;
+            if (freq[i] & 1)
+                mid.push_back(char('a' + i));
+        }
+
+        if (countWays(half) < k)
+            return "";
+
+        string left = "";
+        int len = s.size() / 2;
+
+        for (int pos = 0; pos < len; pos++) {
+
+            for (int c = 0; c < 26; c++) {
+
+                if (half[c] == 0) continue;
+
+                half[c]--;
+
+                long long ways = countWays(half);
+
+                if (ways >= k) {
+                    left.push_back(char('a' + c));
+                    break;
+                }
+
+                k -= ways;
+                half[c]++;
+            }
+        }
+
+        string right = left;
+        reverse(right.begin(), right.end());
+
+        return left + mid + right;
+    }
+};
